2d6343dbbd0af675b0fcfc74f557019c2f9e004d
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-02-06 22:50:36 +0600

    dark correction

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ import Ph from '../assets/ph.jpg'
 
 const About = () => {
     return (
-        <div className="a">
+        <div className="a" >
             <div className="a-left">
                 <div className="a-card bg"></div>
                 <div className="a-card">
