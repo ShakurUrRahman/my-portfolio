bd6f5f72057a7c7b5698ccf8271007a04dc5ccc4
shakururrahman <shakururrahman@gmail.com>
2023-06-07 20:36:11 +0600

    product sliced & added project

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import { products } from '../data';
-import Product from './Product';
-import '../Styles/ProductList.css'
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { products } from "../data";
+import Product from "./Product";
+import "../Styles/ProductList.css";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ProductList = () => {
-    return (
-        <div className="pl">
-            <div className="pl-texts">
-                <h1 className="pl-title">My Projects</h1>
-                <p className="pl-desc">
-                    Here I am introducing you with my various significant projects where I implemented many kinds of tech and framework.
-                </p>
-            </div>
-            <div className="pl-list">
-                {products.map((item, idx) => (
-                    <Product
-                        key={item.id}
-                        item={item}
-                        id={idx}
-                    />
-                ))}
-            </div>
-            <Button style={{ marginTop: "20px" }} variant="contained"><Link style={{ textDecoration: "none", color: "white", padding: "10px" }} to="/projects">All Projects</Link></Button>
-        </div>
-    );
+  const slicedData = products.slice(0, 6);
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">My Projects</h1>
+        <p className="pl-desc">
+          Here I am introducing you with my various significant projects where I
+          implemented many kinds of tech and framework.
+        </p>
+      </div>
+      <div className="pl-list">
+        {slicedData.map((item, idx) => (
+          <Product key={item.id} item={item} id={idx} />
+        ))}
+      </div>
+      <Button style={{ marginTop: "20px" }} variant="contained">
+        <Link
+          style={{ textDecoration: "none", color: "white", padding: "10px" }}
+          to="/projects"
+        >
+          All Projects
+        </Link>
+      </Button>
+    </div>
+  );
 };
 
 export default ProductList;
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,20 +5,16 @@ import "../Styles/Projects.css";
 import Product from "../Components/Product";
 
 function Projects() {
-    return (
-        <div className="projects">
-            <h1> My Personal Projects</h1>
-            <div className="pl-list">
-                {products.map((item, idx) => (
-                    <Product
-                        key={item.id}
-                        item={item}
-                        id={idx}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="projects">
+      <h1 className="project-title"> My Personal Projects</h1>
+      <div className="pl-list">
+        {products.map((item, idx) => (
+          <Product key={item.id} item={item} id={idx} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
