6eb4b9461daa4e4233e4b12f5f4e42078e516d69
ShakurUrRahman <shakururrahman@gmail.com>
2026-03-19 22:47:01 +0600

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import PortfolioMain from "./components/portfolio-main";
 import { getData } from "@/lib/getData";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
 	const data = await getData();
 
